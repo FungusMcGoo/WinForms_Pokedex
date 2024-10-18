5fa867b55dead5fe669338fe973fed12328b5a94
Josh <Josh@FUNGUSMCGOO>
2024-10-18 13:42:00 +0800

    Removed labels, replaced with DataGridView. DGV queries SQL and is able to pull pokemon data from db. Currently functioning incorrectly. Search is fully functioning and can be executed by clicking  "GO" or pressing enter.

--- Pokedex.Designer.vb
+++ Pokedex.Designer.vb
@@ -22,30 +22,22 @@ Partial Class Pokedex
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As DataGridViewCellStyle = New DataGridViewCellStyle()
         cmb_Search = New ComboBox()
         btn_Search = New Button()
         pic_Pokemon = New PictureBox()
         rtxt_Description = New RichTextBox()
-        lbl_HP = New Label()
-        lbl_Attack = New Label()
-        lbl_Defence = New Label()
-        lbl_SpAttack = New Label()
-        lbl_SpDefence = New Label()
-        lbl_Speed = New Label()
         panel_Stats = New Panel()
-        lbl_Generation = New Label()
-        lbl_CatchRate = New Label()
         PictureBox1 = New PictureBox()
         pic_Type1 = New PictureBox()
-        lbl_Class = New Label()
-        lbl_Height = New Label()
-        lbl_Weight = New Label()
-        DataGridView1 = New DataGridView()
+        dgv_Stats = New DataGridView()
+        statName = New DataGridViewTextBoxColumn()
+        statValue = New DataGridViewTextBoxColumn()
         CType(pic_Pokemon, ComponentModel.ISupportInitialize).BeginInit()
         panel_Stats.SuspendLayout()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         CType(pic_Type1, ComponentModel.ISupportInitialize).BeginInit()
-        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(dgv_Stats, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' cmb_Search
@@ -53,217 +45,121 @@ Partial Class Pokedex
         cmb_Search.AutoCompleteMode = AutoCompleteMode.SuggestAppend
         cmb_Search.AutoCompleteSource = AutoCompleteSource.ListItems
         cmb_Search.FormattingEnabled = True
-        cmb_Search.Location = New Point(8, 7)
-        cmb_Search.Margin = New Padding(2)
+        cmb_Search.Location = New Point(11, 12)
         cmb_Search.Name = "cmb_Search"
         cmb_Search.RightToLeft = RightToLeft.No
-        cmb_Search.Size = New Size(452, 23)
+        cmb_Search.Size = New Size(644, 33)
         cmb_Search.TabIndex = 0
         cmb_Search.Text = "Search..."
         ' 
         ' btn_Search
         ' 
-        btn_Search.Location = New Point(463, 7)
-        btn_Search.Margin = New Padding(2)
+        btn_Search.Location = New Point(661, 12)
         btn_Search.Name = "btn_Search"
-        btn_Search.Size = New Size(157, 20)
+        btn_Search.Size = New Size(224, 33)
         btn_Search.TabIndex = 1
         btn_Search.Text = "Go!"
         btn_Search.UseVisualStyleBackColor = True
         ' 
         ' pic_Pokemon
         ' 
-        pic_Pokemon.Location = New Point(8, 31)
-        pic_Pokemon.Margin = New Padding(2)
+        pic_Pokemon.Location = New Point(11, 52)
         pic_Pokemon.Name = "pic_Pokemon"
-        pic_Pokemon.Size = New Size(363, 227)
+        pic_Pokemon.Size = New Size(519, 378)
         pic_Pokemon.TabIndex = 2
         pic_Pokemon.TabStop = False
         ' 
         ' rtxt_Description
         ' 
-        rtxt_Description.Location = New Point(8, 262)
-        rtxt_Description.Margin = New Padding(2)
+        rtxt_Description.Location = New Point(11, 437)
         rtxt_Description.Name = "rtxt_Description"
-        rtxt_Description.Size = New Size(364, 117)
+        rtxt_Description.Size = New Size(518, 192)
         rtxt_Description.TabIndex = 4
         rtxt_Description.Text = ""
         ' 
-        ' lbl_HP
-        ' 
-        lbl_HP.AutoSize = True
-        lbl_HP.Location = New Point(2, 137)
-        lbl_HP.Margin = New Padding(2, 0, 2, 0)
-        lbl_HP.Name = "lbl_HP"
-        lbl_HP.Size = New Size(23, 15)
-        lbl_HP.TabIndex = 5
-        lbl_HP.Text = "HP"
-        ' 
-        ' lbl_Attack
-        ' 
-        lbl_Attack.AutoSize = True
-        lbl_Attack.Location = New Point(2, 152)
-        lbl_Attack.Margin = New Padding(2, 0, 2, 0)
-        lbl_Attack.Name = "lbl_Attack"
-        lbl_Attack.Size = New Size(41, 15)
-        lbl_Attack.TabIndex = 6
-        lbl_Attack.Text = "Attack"
-        ' 
-        ' lbl_Defence
-        ' 
-        lbl_Defence.AutoSize = True
-        lbl_Defence.Location = New Point(2, 167)
-        lbl_Defence.Margin = New Padding(2, 0, 2, 0)
-        lbl_Defence.Name = "lbl_Defence"
-        lbl_Defence.Size = New Size(50, 15)
-        lbl_Defence.TabIndex = 7
-        lbl_Defence.Text = "Defence"
-        ' 
-        ' lbl_SpAttack
-        ' 
-        lbl_SpAttack.AutoSize = True
-        lbl_SpAttack.Location = New Point(2, 182)
-        lbl_SpAttack.Margin = New Padding(2, 0, 2, 0)
-        lbl_SpAttack.Name = "lbl_SpAttack"
-        lbl_SpAttack.Size = New Size(60, 15)
-        lbl_SpAttack.TabIndex = 8
-        lbl_SpAttack.Text = "Sp. Attack"
-        ' 
-        ' lbl_SpDefence
-        ' 
-        lbl_SpDefence.AutoSize = True
-        lbl_SpDefence.Location = New Point(2, 197)
-        lbl_SpDefence.Margin = New Padding(2, 0, 2, 0)
-        lbl_SpDefence.Name = "lbl_SpDefence"
-        lbl_SpDefence.Size = New Size(69, 15)
-        lbl_SpDefence.TabIndex = 9
-        lbl_SpDefence.Text = "Sp. Defence"
-        ' 
-        ' lbl_Speed
-        ' 
-        lbl_Speed.AutoSize = True
-        lbl_Speed.Location = New Point(2, 212)
-        lbl_Speed.Margin = New Padding(2, 0, 2, 0)
-        lbl_Speed.Name = "lbl_Speed"
-        lbl_Speed.Size = New Size(39, 15)
-        lbl_Speed.TabIndex = 10
-        lbl_Speed.Text = "Speed"
-        ' 
         ' panel_Stats
         ' 
-        panel_Stats.Controls.Add(lbl_Generation)
-        panel_Stats.Controls.Add(lbl_CatchRate)
         panel_Stats.Controls.Add(PictureBox1)
         panel_Stats.Controls.Add(pic_Type1)
-        panel_Stats.Controls.Add(lbl_Class)
-        panel_Stats.Controls.Add(lbl_Height)
-        panel_Stats.Controls.Add(lbl_Weight)
-        panel_Stats.Controls.Add(lbl_HP)
-        panel_Stats.Controls.Add(lbl_Attack)
-        panel_Stats.Controls.Add(lbl_Defence)
-        panel_Stats.Controls.Add(lbl_SpAttack)
-        panel_Stats.Controls.Add(lbl_SpDefence)
-        panel_Stats.Controls.Add(lbl_Speed)
-        panel_Stats.Controls.Add(DataGridView1)
+        panel_Stats.Controls.Add(dgv_Stats)
         panel_Stats.Enabled = False
-        panel_Stats.Location = New Point(376, 31)
-        panel_Stats.Margin = New Padding(2)
+        panel_Stats.Location = New Point(537, 52)
         panel_Stats.Name = "panel_Stats"
-        panel_Stats.Size = New Size(244, 227)
+        panel_Stats.Size = New Size(349, 576)
         panel_Stats.TabIndex = 11
-        ' 
-        ' lbl_Generation
-        ' 
-        lbl_Generation.AutoSize = True
-        lbl_Generation.Location = New Point(2, 60)
-        lbl_Generation.Margin = New Padding(2, 0, 2, 0)
-        lbl_Generation.Name = "lbl_Generation"
-        lbl_Generation.Size = New Size(65, 15)
-        lbl_Generation.TabIndex = 20
-        lbl_Generation.Text = "Generation"
-        ' 
-        ' lbl_CatchRate
-        ' 
-        lbl_CatchRate.AutoSize = True
-        lbl_CatchRate.Location = New Point(2, 120)
-        lbl_CatchRate.Margin = New Padding(2, 0, 2, 0)
-        lbl_CatchRate.Name = "lbl_CatchRate"
-        lbl_CatchRate.Size = New Size(64, 15)
-        lbl_CatchRate.TabIndex = 19
-        lbl_CatchRate.Text = "Catch Rate"
         ' 
         ' PictureBox1
         ' 
-        PictureBox1.Location = New Point(-1, 32)
-        PictureBox1.Margin = New Padding(2)
+        PictureBox1.Location = New Point(-1, 53)
         PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(239, 28)
+        PictureBox1.Size = New Size(341, 47)
         PictureBox1.TabIndex = 18
         PictureBox1.TabStop = False
         ' 
         ' pic_Type1
         ' 
-        pic_Type1.Location = New Point(2, 0)
-        pic_Type1.Margin = New Padding(2)
+        pic_Type1.Location = New Point(3, 0)
         pic_Type1.Name = "pic_Type1"
-        pic_Type1.Size = New Size(239, 28)
+        pic_Type1.Size = New Size(341, 47)
         pic_Type1.TabIndex = 17
         pic_Type1.TabStop = False
         ' 
-        ' lbl_Class
+        ' dgv_Stats
         ' 
-        lbl_Class.AutoSize = True
-        lbl_Class.Location = New Point(2, 75)
-        lbl_Class.Margin = New Padding(2, 0, 2, 0)
-        lbl_Class.Name = "lbl_Class"
-        lbl_Class.Size = New Size(77, 15)
-        lbl_Class.TabIndex = 16
-        lbl_Class.Text = "Classification"
+        dgv_Stats.AllowUserToAddRows = False
+        dgv_Stats.AllowUserToDeleteRows = False
+        dgv_Stats.BackgroundColor = SystemColors.Control
+        dgv_Stats.BorderStyle = BorderStyle.None
+        dgv_Stats.CellBorderStyle = DataGridViewCellBorderStyle.None
+        dgv_Stats.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgv_Stats.ColumnHeadersVisible = False
+        dgv_Stats.Columns.AddRange(New DataGridViewColumn() {statName, statValue})
+        DataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = SystemColors.Control
+        DataGridViewCellStyle1.Font = New Font("Segoe UI", 9F)
+        DataGridViewCellStyle1.ForeColor = SystemColors.ControlText
+        DataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = DataGridViewTriState.False
+        dgv_Stats.DefaultCellStyle = DataGridViewCellStyle1
+        dgv_Stats.GridColor = SystemColors.Control
+        dgv_Stats.Location = New Point(1, 210)
+        dgv_Stats.Name = "dgv_Stats"
+        dgv_Stats.ReadOnly = True
+        dgv_Stats.RowHeadersVisible = False
+        dgv_Stats.RowHeadersWidth = 62
+        dgv_Stats.ScrollBars = ScrollBars.None
+        dgv_Stats.Size = New Size(341, 369)
+        dgv_Stats.TabIndex = 13
         ' 
-        ' lbl_Height
+        ' statName
         ' 
-        lbl_Height.AutoSize = True
-        lbl_Height.Location = New Point(2, 90)
-        lbl_Height.Margin = New Padding(2, 0, 2, 0)
-        lbl_Height.Name = "lbl_Height"
-        lbl_Height.Size = New Size(43, 15)
-        lbl_Height.TabIndex = 15
-        lbl_Height.Text = "Height"
+        statName.HeaderText = "statName"
+        statName.MinimumWidth = 8
+        statName.Name = "statName"
+        statName.ReadOnly = True
+        statName.Resizable = DataGridViewTriState.True
+        statName.Width = 150
         ' 
-        ' lbl_Weight
+        ' statValue
         ' 
-        lbl_Weight.AutoSize = True
-        lbl_Weight.Location = New Point(2, 105)
-        lbl_Weight.Margin = New Padding(2, 0, 2, 0)
-        lbl_Weight.Name = "lbl_Weight"
-        lbl_Weight.Size = New Size(45, 15)
-        lbl_Weight.TabIndex = 14
-        lbl_Weight.Text = "Weight"
-        ' 
-        ' DataGridView1
-        ' 
-        DataGridView1.BackgroundColor = SystemColors.ControlLight
-        DataGridView1.BorderStyle = BorderStyle.None
-        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Location = New Point(78, 137)
-        DataGridView1.Margin = New Padding(2)
-        DataGridView1.Name = "DataGridView1"
-        DataGridView1.RowHeadersWidth = 62
-        DataGridView1.Size = New Size(166, 90)
-        DataGridView1.TabIndex = 13
+        statValue.HeaderText = "statValue"
+        statValue.MinimumWidth = 8
+        statValue.Name = "statValue"
+        statValue.ReadOnly = True
+        statValue.Width = 240
         ' 
         ' Pokedex
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(626, 384)
+        ClientSize = New Size(894, 640)
         Controls.Add(rtxt_Description)
         Controls.Add(pic_Pokemon)
         Controls.Add(btn_Search)
         Controls.Add(cmb_Search)
         Controls.Add(panel_Stats)
         FormBorderStyle = FormBorderStyle.Fixed3D
-        Margin = New Padding(2)
         MaximizeBox = False
         MinimizeBox = False
         Name = "Pokedex"
@@ -271,10 +167,9 @@ Partial Class Pokedex
         Text = "Pokedex"
         CType(pic_Pokemon, ComponentModel.ISupportInitialize).EndInit()
         panel_Stats.ResumeLayout(False)
-        panel_Stats.PerformLayout()
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         CType(pic_Type1, ComponentModel.ISupportInitialize).EndInit()
-        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        CType(dgv_Stats, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
 
@@ -282,20 +177,11 @@ Partial Class Pokedex
     Friend WithEvents btn_Search As Button
     Friend WithEvents pic_Pokemon As PictureBox
     Friend WithEvents rtxt_Description As RichTextBox
-    Friend WithEvents lbl_HP As Label
-    Friend WithEvents lbl_Attack As Label
-    Friend WithEvents lbl_Defence As Label
-    Friend WithEvents lbl_SpAttack As Label
-    Friend WithEvents lbl_SpDefence As Label
-    Friend WithEvents lbl_Speed As Label
     Friend WithEvents panel_Stats As Panel
-    Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents lbl_Class As Label
-    Friend WithEvents lbl_Height As Label
-    Friend WithEvents lbl_Weight As Label
     Friend WithEvents pic_Type1 As PictureBox
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents lbl_CatchRate As Label
-    Friend WithEvents lbl_Generation As Label
+    Friend WithEvents dgv_Stats As DataGridView
+    Friend WithEvents statName As DataGridViewTextBoxColumn
+    Friend WithEvents statValue As DataGridViewTextBoxColumn
 
 End Class
--- Pokedex.vb
+++ Pokedex.vb
@@ -10,12 +10,70 @@ Public Class Pokedex
         Using conSQL As New SqlConnection(conStr)
             Dim cmdSQL As New SqlCommand(pkmnquery, conSQL)
             conSQL.Open()
-
             Dim reader As SqlDataReader = cmdSQL.ExecuteReader()
-            While reader.Read()
-                cmb_Search.Items.Add(reader("ID") & " - " & reader("Name"))
-            End While
+
+            'Datatable to store pokemon data
+            Dim dt As New DataTable()
+            dt.Load(reader)
+
+            'Set CB DisplayMember = Name and ValueMember = ID
+            cmb_Search.DataSource = dt
+            cmb_Search.DisplayMember = "Name"
+            cmb_Search.ValueMember = "ID"
+
             conSQL.Close()
         End Using
     End Sub
+
+    Private Sub btn_Search_Click(sender As Object, e As EventArgs) Handles btn_Search.Click
+        If dgv_Stats IsNot Nothing Then
+            dgv_Stats.Refresh()
+        End If
+        '
+        'Ensure is not null
+        If cmb_Search.SelectedValue IsNot Nothing Then
+            Dim selectedPokemonID As Integer = CInt(cmb_Search.SelectedValue)
+        End If
+
+        'Query to retrieve Pokemon details
+        Dim pkmnstatquery As String = "SELECT * FROM pokedex_data WHERE ID = ID"
+
+        'Init SQL connection and datareader
+        Using conSQL As New SqlConnection(conStr)
+            Dim cmdSQL As New SqlCommand(pkmnstatquery, conSQL)
+            conSQL.Open()
+            Dim reader As SqlDataReader = cmdSQL.ExecuteReader()
+
+            'Clear DGV
+            dgv_Stats.Rows.Clear()
+            rtxt_Description.Clear()
+            If reader.Read() Then
+                dgv_Stats.Rows.Add("Generation", reader("Generation").ToString())
+                dgv_Stats.Rows.Add("Classification", reader("Classification").ToString())
+                dgv_Stats.Rows.Add("Height", reader("Height").ToString())
+                dgv_Stats.Rows.Add("Weight", reader("Weight").ToString())
+                dgv_Stats.Rows.Add("Catch Rate", reader("Capture_rate").ToString())
+                dgv_Stats.Rows.Add("HP", reader("HP").ToString())
+                dgv_Stats.Rows.Add("Attack", reader("Attack").ToString())
+                dgv_Stats.Rows.Add("Defence", reader("Defence").ToString())
+                dgv_Stats.Rows.Add("Special Attack", reader("SP_Attack").ToString())
+                dgv_Stats.Rows.Add("Special Defence", reader("SP_Defence").ToString())
+                dgv_Stats.Rows.Add("Speed", reader("Speed").ToString())
+                'rtxt_Description.Text = reader("Description").ToString
+
+            End If
+
+        End Using
+    End Sub
+
+    Private Sub cmb_search_KeyDown(sender As Object, e As KeyEventArgs) Handles cmb_Search.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            'CLEAR DGV
+            dgv_Stats.Refresh()
+            btn_Search.PerformClick()
+        End If
+
+    End Sub
+
+
 End Class
